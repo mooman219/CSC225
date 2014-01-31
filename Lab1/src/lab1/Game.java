df0669078440730495ae47bda01868dea06bd2ee
Joe <mooman219@gmail.com>
2014-01-30 21:06:54 -0500

    Use a better compare

--- Lab1/src/lab1/Game.java
+++ Lab1/src/lab1/Game.java
@@ -6,28 +6,35 @@ import java.util.Scanner;
 public class Game {
 
     public static void main(String[] args) {
-        new Game().startGame();
+        new Game(2).startGame();
     }
 
     public Random random = new Random();
     public Scanner scanner = new Scanner(System.in);
+    public int winThreshold = 2;
+
     public int playerWins = 0;
     public int computerWins = 0;
 
-    public Game() {}
+    public Game(int winThreshold) {
+        this.winThreshold = winThreshold;
+    }
 
     public void startGame() {
-        while (playerWins < 2 && computerWins < 2) {
+        while(Math.max(playerWins, computerWins) < winThreshold) {
             playRound();
         }
+
         System.out.println("The " + (playerWins > computerWins ? "Player" : "Computer") + " has won.");
     }
 
     public void playRound() {
-        int playerInput = getInput();
+        int playerInput = getPlayerAction();
         int computerInput = random.nextInt(3);
+
         System.out.print("The computer is " + getActionName(computerInput) + ". You are " + getActionName(playerInput) + ". ");
-        switch (Integer.compare(playerInput, computerInput)) {
+
+        switch(compareAction(playerInput, computerInput)) {
             case -1:
                 System.out.println("You lose.");
                 computerWins++;
@@ -42,8 +49,24 @@ public class Game {
         }
     }
 
-    public String getActionName(int action) {
-        switch (action) {
+    public int getPlayerAction() {
+        int input = -1;
+
+        while(input < 0 || input > 2) {
+            System.out.print("\nscissor (0), rock (1), paper (2): ");
+            input = scanner.nextInt();
+        }
+
+        return input;
+    }
+
+    public static int compareAction(int player, int computer) {
+        int key = player - computer;
+        return key == 0 ? 0 : key == 1 || key == -2 ? 1 : -1; // Witchcraft
+    }
+
+    public static String getActionName(int action) {
+        switch(action) {
             case 0:
                 return "Scissor";
             case 1:
@@ -53,14 +76,5 @@ public class Game {
             default:
                 return "Error";
         }
-    }
-
-    public int getInput() {
-        int input = -1;
-        while (input < 0 || input > 2) {
-            System.out.print("\nscissor (0), rock (1), paper (2): ");
-            input = scanner.nextInt();
-        }
-        return input;
     }
 }
