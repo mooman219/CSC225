5131cdfc99fae72514325cedf0dd961cf87fe4a0
Joe <mooman219@gmail.com>
2014-04-29 15:46:52 -0400

    netbeans fluff

--- Lab3/src/Calculator.java
+++ Lab3/src/Calculator.java
@@ -11,8 +11,6 @@ import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
 
 public class Calculator extends JFrame {
@@ -191,26 +189,6 @@ public class Calculator extends JFrame {
     }
 
     public static void main(String args[]) {
-        try {
-            for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch(ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CalculatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CalculatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CalculatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CalculatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        /*
-         * Create and display the form
-         */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new CalculatorGUI().setVisible(true);
